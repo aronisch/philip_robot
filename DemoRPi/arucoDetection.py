1ccfdba2438ffd134b372c47c19305081721d775
Arthur Rönisch <a.ronisch@outlook.com>
2019-12-01 18:55:40 -0500

    Add aruco markers and opencv detection example

---
+++ DemoRPi/arucoDetection.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+ 
+ 
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4,480)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    #print(frame.shape) #480x640
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters =  aruco.DetectorParameters_create()
+    
+    #lists of ids and the corners beloning to each id
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    print(corners)
+  
+    gray = aruco.drawDetectedMarkers(gray, corners)
+    
+    if len(corners):
+        for i in range(len(corners)):
+            if(ids[i]==6):
+                middlepoint = int((corners[i][0][0][0]+corners[i][0][1][0])/2)
+                cv2.line(gray,(middlepoint,0),(middlepoint,720),(255,0,0),1)
+        
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
