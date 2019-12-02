485dae8c0159b1aed93c76b197219381c9ab23e7
Arthur Rönisch <a.ronisch@outlook.com>
2019-12-02 11:21:57 -0500

    Encode strings for pyserial write

--- DemoRPi/teensy_controller.py
+++ DemoRPi/teensy_controller.py
@@ -9,27 +9,27 @@ class TeensyController:
         
     def set_velocities(self, linear_vel, angular_vel):
         vel_packet = "L" + linear_vel + "A" + angular_vel + "\n"
-        self.ser.write(vel_packet)
+        self.ser.write(vel_packet.encode('utf-8'))
         
     def close_plate_holder(self):
-        self.ser.write("P0\n")
+        self.ser.write("P0\n".encode('utf-8'))
     
     def open_plate_holder(self):
-        self.ser.write("P1\n")
+        self.ser.write("P1\n".encode('utf-8'))
     
     def close_gripper(self):
-        self.ser.write("G0\n")
+        self.ser.write("G0\n".encode('utf-8'))
     
     def open_gripper(self):
-        self.ser.write("G1\n")
+        self.ser.write("G1\n".encode('utf-8'))
         
     def set_gripper_grabbing_position(self):
-        self.ser.write("R0\n")
+        self.ser.write("R0\n".encode('utf-8'))
         
     def set_gripper_releasing_position(self):
-        self.ser.write("R1\n")
+        self.ser.write("R1\n".encode('utf-8'))
         
     def obstacle_present(self):
-        self.ser.write("O\n")
+        self.ser.write("O\n".encode('utf-8'))
         return self.read() == "T"
     
